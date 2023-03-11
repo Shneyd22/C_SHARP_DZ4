16e37f47f0716770d06a444e2bf03e449fec406e
Sergei Schneidmiller <5797221@gmail.com>
2023-03-11 12:42:49 +0300

    dz4 ready

--- Program.cs
+++ Program.cs
@@ -13,26 +13,3 @@ while (i <= N)
         i++;
         }
 }
-
-// int[] numbers = new int[N] {};
-// int index = 0;
-// numbers[index] = i;
-
-// Console.WriteLine("Введите первое число");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int num2 = Convert.ToInt32(Console.ReadLine());  
-// Console.WriteLine("Введите третье число");
-// int num3 = Convert.ToInt32(Console.ReadLine());
-// int[] numbers = new int[3] {num1, num2, num3};
-// int index = 0;
-// int max = 0;
-// while (index < 3)
-// {
-//     if (numbers[index] > max)
-//         {
-//             max = numbers[index];
-//             index++;
-//         }
-// }
-// Console.WriteLine("Максимальное число: " + max);
